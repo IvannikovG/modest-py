6a1402b7d47ac63aad9772e0e61e8e6e89435bce
Georgii <georgii@localhost.localdomain>
2020-04-12 18:07:19 +0200

    Adjust top level model for simulation

--- modestpy/estim/modelFMpy.py
+++ modestpy/estim/modelFMpy.py
@@ -5,3 +5,61 @@ from __future__ import print_function
 
 import logging
 from modelFMpyBase import Model as FMPyModel
+
+VERBOSE = True
+
+class Model(object):
+    def __init__(self, fmu_path, opts=None):
+        self.logger = logging.getLogger(type(self).__name__)
+
+        self.model = FMPyModel(fmu_path, opts=opts)
+
+        #Logging: >TODO<
+
+        # Simulation count
+        self.sim_count = 0
+
+        def set_input(self, df, exclude=list()):
+            """ Sets inputs.
+
+            :param df: Dataframe, time given in seconds
+            :param exclude: list of strings, names of columns to be excluded
+            :return: None
+            """
+            self.model.inputs_from_df(df, exclude)
+
+        def set_param(self, df):
+            """ Sets parameters. It is possible to set only a subset of model parameters.
+
+            :param df: Dataframe with header and a single row of data
+            :return: None
+            """
+            self.model.parameters_from_df(df)
+
+        def set_outputs(self, outputs):
+            """ Sets output variables.
+
+            :param outputs: list of strings
+            :return: None
+            """
+            self.model.specify_outputs(outputs)
+
+        def simulate(self, com_points=None):
+            # TODO: com_points should be adjusted to the number of samples
+            self.sim_count += 1
+            self.info('Simulation count = ' + str(self.sim_count))
+            return self.model.simulate(com_points=com_points)
+
+        def info(self, txt):
+            class_name = self.__class__.__name__
+            if VERBOSE:
+                if isinstance(txt, str):
+                    print('[' + class_name + '] ' + txt)
+                else:
+                    print('[' + class_name + '] ' + repr(txt))
+
+
+model = Model('/home/georgii/Documents/modest-py/modestpy/fmi/Simple2R1C.fmu')
+model.model.specify_outputs(['Ti1', 'Ti2'])
+model.model.specify_input('/home/georgii/Documents/modest-py/modestpy/fmi/inputs.csv')
+print(model.model.simulate())
--- modestpy/estim/modelFMpyBase.py
+++ modestpy/estim/modelFMpyBase.py
@@ -57,6 +57,34 @@ class Model(object):
             for col in df:
                 self.parameter_df[col] = df[col]
 
+    def inputs_from_df(self, df, exclude=list()):
+        """
+        Reads inputs from dataframe.
+
+        Index must be named 'time' and given in seconds.
+        The index name assertion check is implemented to avoid
+        situations in which a user read DataFrame from csv
+        and forgot to use ``DataFrame.set_index(column_name)``
+        (it happens quite often...).
+
+        :param df: DataFrame
+        :param exclude: list of strings, names of columns to be omitted
+        :return:
+        """
+        assert df.index.name == 'time', "Index name ('{}') different " \
+                                        "than 'time'! " \
+                                        "Are you sure you assigned index " \
+                                        "correctly?".format(df.index.name)
+        self.timeline = df.index.values
+        self.start = self.timeline[0]
+        self.end = self.timeline[-1]
+
+        for col in df:
+            if col not in exclude:
+                if col not in self.input_names:
+                    self.input_names.append(col)
+                    self.input_values.append(df[col].values)
+
     def specify_outputs(self, outputs):
         """
         Specifies names of output variables
