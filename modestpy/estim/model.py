129d9d323378ca1a3a7854aca61c15b68c0558db
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-04-14 17:05:55 +0200

    Fix incorrect relative import

--- modestpy/estim/model.py
+++ modestpy/estim/model.py
@@ -4,7 +4,7 @@ from __future__ import division
 from __future__ import print_function
 
 import logging
-from fmi.model import Model as FMPyModel
+from modestpy.fmi.model import Model as FMPyModel
 
 VERBOSE = True
 
