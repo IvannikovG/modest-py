90cb12dd578ce620a599f4e7ac4a2c461c4fef5a
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-04-16 10:21:08 +0200

    Restore original file as much as possible
    Motivation: keep changes minimal for better readability/traceability of 
    changes

--- modestpy/estim/model.py
+++ modestpy/estim/model.py
@@ -1,23 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2017, University of Southern Denmark
+All rights reserved.
+This code is licensed under BSD 2-clause license.
+See LICENSE file in the project root for license terms.
+"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-
 import logging
-from modestpy.fmi.model import Model as FMPyModel
 
+from modestpy.fmi.model import Model as FmiModel
+
+# PyFmi log level (controls the amount of information saved to the log file)
+# (watch out for the file writing overhead)
+FMI_NOTHING = 0
+FMI_FATAL = 1
+FMI_ERROR = 2
+FMI_WARNING = 3
+FMI_INFO = 4
+FMI_VERBOSE = 5
+FMI_DEBUG = 6
+FMI_ALL = 7
+
+# Printing on the screen
 VERBOSE = True
 
 
 class Model(object):
+    """ Model for static parameter estimation """
     def __init__(self, fmu_path, opts=None):
         self.logger = logging.getLogger(type(self).__name__)
 
-        self.model = FMPyModel(fmu_path, opts=opts)
+        self.model = FmiModel(fmu_path, opts=opts)
 
-        # Logging: >TODO<
+        # Log level
+        try:
+            self.model.model.set_log_level(FMI_WARNING)
+        except AttributeError as e:
+            self.logger.error(e.message)
+            self.logger.error('Proceeding with standard log level...')
 
         # Simulation count
         self.sim_count = 0
@@ -50,7 +76,7 @@ class Model(object):
     def simulate(self, com_points=None):
         # TODO: com_points should be adjusted to the number of samples
         self.sim_count += 1
-        self.info('Simulation count CYKA!! = ' + str(self.sim_count))
+        self.info('Simulation count = ' + str(self.sim_count))
         return self.model.simulate(com_points=com_points)
 
     def info(self, txt):
